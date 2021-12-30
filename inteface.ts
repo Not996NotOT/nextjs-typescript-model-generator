6aefa92a6427269271d4785f49ff2f2b94b9feb2
zhangxing <zhangxing@zhangxingdeMacBook-Pro.local>
2021-12-30 15:56:54 +0800

    init

---
+++ inteface.ts
@@ -0,0 +1,167 @@
+export interface Itest { 
+	/**
+	* @description jwtToken
+	*/
+	jwtToken:string;
+	/**
+	* @description registerType
+	*/
+	registerType:string;
+	/**
+	* @description username
+	*/
+	username:string;
+	/**
+	* @description userId
+	*/
+	userId:number;
+	/**
+	* @description userType
+	*/
+	userType:string;
+	/**
+	* @description realname
+	*/
+	realname:string;
+	/**
+	* @description phone
+	*/
+	phone:string;
+	/**
+	* @description email
+	*/
+	email:string;
+	/**
+	* @description status
+	*/
+	status:number;
+	/**
+	* @description statusKyc
+	*/
+	statusKyc:number;
+	/**
+	* @description statusKycLevel
+	*/
+	statusKycLevel:number;
+	/**
+	* @description statusGoogle
+	*/
+	statusGoogle:number;
+	/**
+	* @description phoneVerify
+	*/
+	phoneVerify:number;
+	/**
+	* @description emailVerify
+	*/
+	emailVerify:number;
+	/**
+	* @description emailSecurity
+	*/
+	emailSecurity:number;
+	/**
+	* @description phoneSecurity
+	*/
+	phoneSecurity:number;
+	/**
+	* @description googleSecurity
+	*/
+	googleSecurity:number;
+	/**
+	* @description tranPassword
+	*/
+	tranPassword:number;
+	/**
+	* @description vipLevel
+	*/
+	vipLevel:string;
+	/**
+	* @description merchant
+	*/
+	merchant:string;
+	/**
+	* @description countryName
+	*/
+	countryName:null;
+	/**
+	* @description idType
+	*/
+	idType:null;
+	/**
+	* @description idNumber
+	*/
+	idNumber:null;
+	/**
+	* @description otcPhoneAdvise
+	*/
+	otcPhoneAdvise:number;
+	/**
+	* @description otcEmailAdvise
+	*/
+	otcEmailAdvise:number;
+	/**
+	* @description symbol
+	*/
+	symbol:null;
+	/**
+	* @description language
+	*/
+	language:string;
+	/**
+	* @description currency
+	*/
+	currency:null;
+	/**
+	* @description tickerColor
+	*/
+	tickerColor:null;
+	/**
+	* @description tickerRefresh
+	*/
+	tickerRefresh:null;
+	/**
+	* @description orderConfirm
+	*/
+	orderConfirm:null;
+	/**
+	* @description token
+	*/
+	token:null;
+	/**
+	* @description hint
+	*/
+	hint:null;
+	/**
+	* @description reason
+	*/
+	reason:null;
+	/**
+	* @description verifyType
+	*/
+	verifyType:null;
+	/**
+	* @description verifyCode
+	*/
+	verifyCode:null;
+	/**
+	* @description needVerify
+	*/
+	needVerify:boolean;
+	/**
+	* @description expireSeconds
+	*/
+	expireSeconds:number;
+	/**
+	* @description nickname
+	*/
+	nickname:string;
+	/**
+	* @description ownerId
+	*/
+	ownerId:string;
+	/**
+	* @description photo
+	*/
+	photo:string;
+}
+    
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,167 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { makeAutoObservable } from "mobx";
+import { observer, useLocalObservable } from "mobx-react-lite";
+import styles from "./index.module.scss";
+
+class CodeController {
+  className;
+  interfaceName;
+  jsonCode;
+  classCode;
+  intefaceCode;
+  constructor() {
+    makeAutoObservable(this);
+  }
+  convertJsonToOther() {
+    console.log("json", this.jsonCode);
+    const jObject = JSON.parse(this.jsonCode);
+    console.log("jobject", jObject);
+    this.convertToInterface(jObject);
+    this.convertToClass(jObject);
+  }
+
+  convertToInterface(jObject) {
+    let str = `export interface ${this.interfaceName} { \n`;
+    let keys = Object.getOwnPropertyNames(jObject);
+    if (keys.length > 0) {
+      keys.forEach((key) => {
+        str += `\t/**\n`;
+        str += `\t* @description ${key}\n`;
+        str += `\t*/\n`;
+        str += `\t${key}:${this.getPropertyType(jObject[key])};\n`;
+      });
+    }
+    str += `}
+    `;
+    console.log(str);
+    this.intefaceCode = str;
+  }
+
+  convertToClass(jObject) {
+    let str = `import { AbsBaseDTO } from "../../infrastructure/abstract/models/BaseModel";\n`;
+    str += `import { ${this.interfaceName} } from "../../interface/models/dto/${this.interfaceName}";\n`;
+    str += `export class ${this.className} extends AbsBaseDTO implements ${this.interfaceName} { \n`;
+    let keys = Object.getOwnPropertyNames(jObject);
+    if (keys.length > 0) {
+      keys.forEach((key) => {
+        str += `\t${key}:${this.getPropertyType(jObject[key])};\n`;
+      });
+      str += `\tconstructor(params?: { \n`;
+      keys.forEach((key) => {
+        str += `\t\t${key}?:${this.getPropertyType(jObject[key])};\n`;
+      });
+      str += `\t}) { \n`;
+      str += `\t\tsuper();\n`;
+      keys.forEach((key) => {
+        str += `\t\tthis.${key} = params?.${key} ?? ${this.getPropertyDeafaultValue(
+          jObject[key]
+        )};\n`;
+      });
+      str += `\t}\n`;
+      str += `}`;
+      console.log("classCode", this.classCode);
+      this.classCode = str;
+    }
+  }
+
+  getPropertyType(propertyValue) {
+    console.log(typeof propertyValue);
+    switch (typeof propertyValue) {
+      case "boolean":
+        return "boolean";
+      case "number":
+        return "number";
+      case "string":
+        return "string";
+      case "undefined":
+        return "undefined";
+      case "object":
+        return this.getComplexPropertyType(propertyValue);
+      default:
+        return "undefined";
+    }
+  }
+  getComplexPropertyType(propertyValue) {
+    switch (propertyValue) {
+      case null:
+        return "null";
+      default:
+        return "undefined";
+    }
+  }
+
+  getPropertyDeafaultValue(propertyValue) {
+    console.log(typeof propertyValue);
+    switch (typeof propertyValue) {
+      case "boolean":
+        return "false";
+      case "number":
+        return "0";
+      case "string":
+        return '""';
+      case "undefined":
+        return "undefined";
+      case "object":
+        return this.getComplexPropertyDeafaultValue(propertyValue);
+      default:
+        return "undefined";
+    }
+  }
+
+  getComplexPropertyDeafaultValue(propertyValue) {
+    switch (propertyValue) {
+      case null:
+        return "null";
+      default:
+        return "undefined";
+    }
+  }
+}
+
+const CodeView = observer(() => {
+  const controller = useLocalObservable(() => new CodeController());
+  return (
+    <div className={styles.main}>
+      <div className={styles.title}>
+        <span>类名字</span>
+        <input
+          value={controller.className}
+          onChange={(e) => {
+            controller.className = e.target.value;
+            controller.interfaceName = `I${e.target.value}`;
+            console.log(controller.interfaceName);
+          }}
+        ></input>
+        <span>接口名称</span>
+        <input
+          value={controller.interfaceName}
+          onChange={(e) => (controller.interfaceName = e.target.value)}
+        ></input>
+        <button onClick={() => controller.convertJsonToOther()}>
+          生成代码
+        </button>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.left}>
+          json
+          <textarea
+            onChange={(e) => (controller.jsonCode = e.target.value)}
+          ></textarea>
+        </div>
+        <div className={styles.right}>
+          inteface
+          <div className={styles.inteface}>
+            <textarea value={controller.intefaceCode}></textarea>
+          </div>
+          class
+          <div className={styles.class}>
+            <textarea value={controller.classCode}></textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+  return <CodeView />;
 }
